2087f2430e6de3ad2a22651b96eb282e5bf67468
James Elsey <james.elsey@gmail.com>
2012-08-21 13:30:43 +0100

    Added class for interface implementation

---
+++ src/com/jameselsey/demo/scjp/declarations_initialisation_scoping/ImplementingAnInterface.java
@@ -0,0 +1,46 @@
+package com.jameselsey.demo.scjp.declarations_initialisation_scoping;
+
+/**
+ * Author:  JElsey
+ * Date:    20/08/2012
+ *
+ * A quick example to show how an interface can be created, and more importantly how it is implemented.
+ */
+public class ImplementingAnInterface
+{
+    public static void main(String[] args)
+    {
+        // Notice the polymorphism here once again, how car IS-A driveable
+        Driveable d1 = new Car();
+        Driveable d2 = new Motorcycle();
+        // The implementations provide the drive() method body.
+        d1.drive();
+        d2.drive();
+    }
+}
+
+/*
+ Create an interface, along with its methods. Think of it like a contract, anything that wants to become Driveable MUST
+ implement the drive method, otherwise the contract is not fulfilled and the compiler will complain!
+  */
+interface Driveable
+{
+    public void drive();
+}
+
+class Car implements Driveable
+{
+    @Override
+    public void drive()
+    {
+        System.out.println("Super charged v8 goes VROOOOM!!");
+    }
+}
+class Motorcycle implements Driveable
+{
+    @Override
+    public void drive()
+    {
+        System.out.println("Front rears up, wheelie down the highway!");
+    }
+}
