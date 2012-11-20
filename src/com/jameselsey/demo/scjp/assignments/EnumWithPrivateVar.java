80821fc9f3f2d97c817f45088dd076a8ff65d8d9
James Elsey <james.elsey@gmail.com>
2012-11-20 20:00:30 +0000

    Tidied up enum using private variables

--- src/com/jameselsey/demo/scjp/assignments/EnumWithPrivateVar.java
+++ src/com/jameselsey/demo/scjp/assignments/EnumWithPrivateVar.java
@@ -3,25 +3,32 @@ package com.jameselsey.demo.scjp.assignments;
 /**
  * Author:  JElsey
  * Date:    11/10/2012
+ *
+ * Even though variables on enums are declared private, they can still be accessed through the enum
  */
-public enum EnumWithPrivateVar
+public class EnumWithPrivateVar
 {
-    NORTH('N'), WEST('W'), EAST('W'), SOUTH('S');
-
-    private char initial;
-
-    EnumWithPrivateVar(char initial)
-    {
-        this.initial = initial;
-    }
-}
-
-class Runner
-{
-    static EnumWithPrivateVar myEnum = EnumWithPrivateVar.NORTH;
-
     public static void main(String[] args)
     {
-//        System.out.println("" + myEnum.NORTH.initial);
+        System.out.println("" + TheEnum.NORTH.initial);
+
+        for(TheEnum e : TheEnum.values()){
+            System.out.print("The enum is " + e.name());
+            System.out.print(" initial is " + e.initial);
+            System.out.println();
+        }
+    }
+
+    enum TheEnum
+    {
+        NORTH('N'), WEST('W'), EAST('W'), SOUTH('S');
+
+        private char initial;
+
+        // This has to be default or private, otherwise a compilation error will occur
+        TheEnum(char initial)
+        {
+            this.initial = initial;
+        }
     }
 }
